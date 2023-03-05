24878f12e8dc29a6ec07f161185fa86bcc63c266
Alexeeva Ekaterina <kat26031995@mail.ru>
2023-03-05 21:21:36 +0300

    Решение задачи 8

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая 
+// на вход принимает два числа и выдаёт, 
+// какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+Console.WriteLine("Введите число: a");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число: b");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 > num2)
+{
+    Console.WriteLine("Первое число максимальное");
+}
+else 
+if (num1 < num2)
+{
+    Console.WriteLine("Второе число максимальное");
+}
---
+++ Task8/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая на вход 
+// принимает число (N), а на выходе 
+// показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number >= 2)
+{
+    for (int i = 2; i <= number; i = i + 2)
+    {
+    Console.Write(i + " ");
+    }
+}

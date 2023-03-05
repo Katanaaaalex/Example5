843e5c9acb44caa31fa20990ebcb9ce84ca5be57
Alexeeva Ekaterina <kat26031995@mail.ru>
2023-03-05 23:25:01 +0300

    Решение задачи 4

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -1,19 +1,24 @@
-﻿// 4. Напишите программу, которая на вход 
-// принимает число и выдаёт, 
-// является ли число чётным 
-// (делится ли оно на два без остатка).
-// 4 -> да
-// -3 -> нет
-// 7 -> нет
+﻿// 4. Напишите программу, которая принимает 
+// на вход три числа и выдаёт максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+
 
 Console.WriteLine("Введите число: a");
-int num = Convert.ToInt32(Console.ReadLine());
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число: b");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число: c");
+int num3 = Convert.ToInt32(Console.ReadLine());
 
-if ((num % 2 )== 0)
-{
-    Console.WriteLine("Да");
-}
-else 
-{
-    Console.WriteLine("Нет");
-}
+int max = num1;
+
+if (num1 > max) max = num1;
+if (num2 > max) max = num2;
+if (num3 > max) max = num3;
+
+Console.Write("max = ");
+Console.WriteLine(max);
